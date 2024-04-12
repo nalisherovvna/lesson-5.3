75b824900f2291a776dd520562fb2110980f7456
nalisherovvna <mushknasiba@gmail.com>
2024-04-12 16:52:26 +0500

    it was my homework

---
+++ main.js
@@ -0,0 +1,27 @@
+// // let mkatab = {
+// //     name: 'xona',
+// //     age: 'fan',
+// //     gvardiya: 'gvardiya'
+// // }
+
+// // for (let key in mkatab) {
+// //     // console.log(mkatab[key][value]);
+// //     text += mkatab[x];
+// // }
+
+// const person = {fname:"John", lname:"Doe", age:25};
+
+// let text = "";
+// for (let x in person) {
+//   text += person[x];
+// }
+
+
+let person = {
+    name: "John",
+    age: 30,
+    gender: "New York"
+}
+for (const key in person) {
+    console.log(key + ":" + person[key]);
+}
